30f2f398f909709f2c86709ea2772a57cba49d79
Andrew Mellinger <30724972+sei-amellinger@users.noreply.github.com>
2022-08-22 12:30:56 -0400

    Refactored and exposed seed. (#1)
    * Removed junk.
    
    * Refactored. Add seed arg. Updated README.
    
    * Made a few revisions to README.md
    
    Co-authored-by: Nick Winski <nwinski@sei.cmu.edu>

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## Introduction
-A lot of wonderful datasets are now available online, such as COCO or Imagenet. which is challenging the limits of computer vision. But it's not easy for us to do some small experiments with such a large number of images to quickly test the validity of algorithmn. For this reason, I created a small dataset named "yymnist" to do both classification and object detection.
+A lot of wonderful datasets are now available online, such as COCO or Imagenet. These datasets help push the limits of 
+computer vision. But it's not easy for us to do some small experiments with such a large number of images to quickly 
+test the validity of algorithm. For this reason, I created a small dataset named "yymnist" to do both classification 
+and object detection.
 
 
 | classification | object detection |
@@ -12,12 +15,13 @@ Clone this repo and install some dependencies
 
 ```
 $ pip install opencv-python
-$ git clone https://github.com/YunYang1994/yymnist.git
+$ git clone https://github.com/cmu-sei/yymnist.git
 ```
 
 ## Classification
 
-If you want to use this dataset for classification, just use `mnist` folder. Each image in the folder starts with a name of its label. For example:
+If you want to use this dataset for classification, use the `mnist` folder. The filename of every image starts with 
+its label. For example:
 
 ```
 mnist/train/0_32970.pgm -> 0
@@ -29,12 +33,24 @@ mnist/train/5_12156.pgm -> 5
 $ python yymnist/make_data.py
 $ python yymnist/show_image.py # [option]
 ```
-you will defaultly get 1000 pictures in the folder `./yymnist/Images` and a `label.txt` in the `./yymnist/`. As for `label.txt`
+Running `make_data.py` will produce 1000 images in the `./yymnist/Images` directory and a `label.txt` file in the 
+`./yymnist/` directory. The resulting images can be used as an object detection dataset. The `label.txt` file describes 
+the images that were produced. Each line of `label.txt` adheres to the following format:
 
 ```
 # image_path x_min, y_min, x_max, y_max, class_id  x_min, y_min ,..., class_id 
 ```
 
+`image_path` indicates the path to the image file. The remaining data describes the ground truth location(s) of any 
+objects in the image. Bounding box coordinates of the object in the format (x_min, y_min, x_max, y_max) are provided, 
+along with an integer indicating which class the object belongs to.
 
+To get a reproducible data set (so others can generate the same data using the same code version) provide the `--seed` 
+switch to `make_data.py` along with an unsigned integer seed value, e.g. an integer in the range 0 to 2**32-1. 
+For example:
+
+```
+$ python yymnist/make_data.py --seed 1234
+```
 
 
--- make_data.py
+++ make_data.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # coding=utf-8
-#================================================================
+# ================================================================
 #   Copyright (C) 2019 * Ltd. All rights reserved.
 #
 #   Editor      : VIM
@@ -9,7 +9,7 @@
 #   Created date: 2019-07-12 20:53:30
 #   Description :
 #
-#================================================================
+# ================================================================
 
 import os
 import cv2
@@ -17,52 +17,31 @@ import numpy as np
 import shutil
 import random
 import argparse
+import sys
 
-'''
-#================================================================
-Begin Functions by Benjamin Commeau
-#================================================================
-'''
 
-'''
-Edited on March 17, 2022
+# ================================================================
+# Changelog
+# 2022-08-17 - sei-amellinger - Exposed setting the seed value via argparse.
 
-The functions random_numpy(*args) and random_python(random_type,*args)
-act as handlers for calling the local generators _rng_numpy and 
-_rng_python with user specified random seeds seed_numpy and seed_python.
+# ================================================================
 
-All code that has been replaced by random_numpy and randon_python are 
-commented with "Benjamin Commeau's edit"
-
-I discovered from reading other documents online that calling the global random functions, such as np.random.seed(seed_numpy), can potentially use the same seed for other modules calling the global random functions. So, I implemented local generators instead, to prevent this module effect other module
-calls.
-
-Even though the local random generators are called as global variables 
-inside their respective functions, these local random generators do not
-effect the global or other local generators in other modules.
-'''
-
-seed_numpy = 2**15-1
-_rng_numpy = np.random.default_rng(seed_numpy)
-def random_numpy(*args):
-    global _rng_numpy
-    return _rng_numpy.integers(*args)
-
-seed_python = 2**15-1
-_rng_python = random.Random(seed_python)
-def random_python(random_type, *args):
-    global _rng_python
-    if random_type == 'choice':
-        return _rng_python.choice(*args)
-    elif random_type == 'randint':
-        return _rng_python.randint(*args)
+# 2022-08-17 - added randomizer setup function
+def setup_randomizer(seed):
+    global randomizer
+    if seed is None:
+        seed = random.randint(0, 2 ** 32 - 1)
     else:
-        return None
-'''
-#================================================================
-End Functions by Benjamin Commeau
-#================================================================
-'''
+        if seed > 2 ** 32 - 1:
+            print("The random seed must be and unsigned 32 bit integer. Exiting")
+            sys.exit(-1)
+
+    print(f"Using random seed: {seed}")
+    randomizer.seed(seed)
+
+
+# ================================================================
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--images_num", type=int, default=1000)
@@ -72,21 +51,34 @@ parser.add_argument("--labels_txt", type=str, default="./yymnist/labels.txt")
 parser.add_argument("--small", type=int, default=3)
 parser.add_argument("--medium", type=int, default=6)
 parser.add_argument("--big", type=int, default=3)
+# sei-amellinger 2022-08-17
+parser.add_argument("--seed", type=int, default=None,
+                    help="Seed to use for randomization. Must be 32 bit unsigned for numpy. "
+                         "By default, one will be made for you.")
+
 flags = parser.parse_args()
+
+# sei-amellinger 2022-08-17
+# We use a global randomizer instance instead of random
+randomizer = random.Random()
+setup_randomizer(flags.seed)
 
 SIZE = flags.image_size
 
 if os.path.exists(flags.images_path): shutil.rmtree(flags.images_path)
 os.mkdir(flags.images_path)
 
-image_paths  = [os.path.join(os.path.realpath("."), "./yymnist/mnist/train/" + image_name) for image_name in os.listdir("./yymnist/mnist/train")]
-image_paths += [os.path.join(os.path.realpath("."), "./yymnist/mnist/test/"  + image_name) for image_name in os.listdir("./yymnist/mnist/test")]
+image_paths = [os.path.join(os.path.realpath("."), "./yymnist/mnist/train/" + image_name) for image_name in
+               os.listdir("./yymnist/mnist/train")]
+image_paths += [os.path.join(os.path.realpath("."), "./yymnist/mnist/test/" + image_name) for image_name in
+                os.listdir("./yymnist/mnist/test")]
+
 
 def compute_iou(box1, box2):
     """xmin, ymin, xmax, ymax"""
 
-    A1 = (box1[2] - box1[0])*(box1[3] - box1[1])
-    A2 = (box2[2] - box2[0])*(box2[3] - box2[1])
+    A1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    A2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
 
     xmin = max(box1[0], box2[0])
     ymin = max(box1[1], box2[1])
@@ -94,28 +86,24 @@ def compute_iou(box1, box2):
     ymax = min(box1[3], box2[3])
 
     if ymin >= ymax or xmin >= xmax: return 0
-    return  ((xmax-xmin) * (ymax - ymin)) / (A1 + A2)
+    return ((xmax - xmin) * (ymax - ymin)) / (A1 + A2)
 
 
-def make_image(data, image_path, ratio=1):
-
+def make_image(data, image_path, ratio:float=1.0):
     blank = data[0]
     boxes = data[1]
     label = data[2]
 
     ID = image_path.split("/")[-1][0]
     image = cv2.imread(image_path)
-    image = cv2.resize(image, (int(28*ratio), int(28*ratio)))
+    image = cv2.resize(image, (int(28 * ratio), int(28 * ratio)))
     h, w, c = image.shape
 
     while True:
-        '''
-        # Benjamin Commeau's edit:
-        xmin = np.random.randint(0, SIZE-w, 1)[0]
-        ymin = np.random.randint(0, SIZE-h, 1)[0]
-        '''
-        xmin = random_numpy(0, SIZE-w, 1)[0]
-        ymin = random_numpy(0, SIZE-h, 1)[0]
+        # 2022-08-17 - Switched to seeded randomizer
+        # NOTE: We switched to using a python randomizer instead of numpy
+        xmin = randomizer.randint(0, SIZE - w)
+        ymin = randomizer.randint(0, SIZE - h)
 
         xmax = xmin + w
         ymax = ymin + h
@@ -136,71 +124,51 @@ def make_image(data, image_path, ratio=1):
     # cv2.rectangle(blank, (xmin, ymin), (xmax, ymax), [0, 0, 255], 2)
     return blank
 
+
 with open(flags.labels_txt, "w") as wf:
     image_num = 0
     while image_num < flags.images_num:
-        image_path = os.path.realpath(os.path.join(flags.images_path, "%06d.jpg" %(image_num+1)))
+        image_path = os.path.realpath(os.path.join(flags.images_path, "%06d.jpg" % (image_num + 1)))
         annotation = image_path
         blanks = np.ones(shape=[SIZE, SIZE, 3]) * 255
-        bboxes = [[0,0,1,1]]
+        bboxes = [[0, 0, 1, 1]]
         labels = [0]
         data = [blanks, bboxes, labels]
         bboxes_num = 0
 
         # small object
         ratios = [0.5, 0.8]
-        '''
-        # Benjamin Commeau's edit:
-        N = random.randint(0, flags.small)
-        '''
-        N = random_python('randint', 0, flags.small)
+        # 2022-08-17 - Switched to seeded randomizer
+        N = randomizer.randint(0, flags.small)
 
-        if N !=0: bboxes_num += 1
+        if N != 0: bboxes_num += 1
         for _ in range(N):
-            '''
-            # Benjamin Commeau's edit:
-            ratio = random.choice(ratios)
-            idx = random.randint(0, 54999)
-            '''
-            ratio = random_python('choice', ratios)
-            idx = random_python('randint', 0, 54999)
+            # 2022-08-17 - Switched to seeded randomizer
+            ratio = randomizer.choice(ratios)
+            idx = randomizer.randint(0, 54999)
 
             data[0] = make_image(data, image_paths[idx], ratio)
 
         # medium object
         ratios = [1., 1.5, 2.]
-        '''
-        # Benjamin Commeau's edit:
-        N = random.randint(0, flags.medium)
-        '''
-        N = random_python('randint', 0, flags.medium)
-        if N !=0: bboxes_num += 1
+        # 2022-08-17 - Switched to seeded randomizer
+        N = randomizer.randint(0, flags.medium)
+        if N != 0: bboxes_num += 1
         for _ in range(N):
-            '''
-            # Benjamin Commeau's edit:
-            ratio = random.choice(ratios)
-            idx = random.randint(0, 54999)
-            '''
-            ratio = random_python('choice', ratios)
-            idx = random_python('randint', 0, 54999)
+            # 2022-08-17 - Switched to seeded randomizer
+            ratio = randomizer.choice(ratios)
+            idx = randomizer.randint(0, 54999)
             data[0] = make_image(data, image_paths[idx], ratio)
 
         # big object
         ratios = [3., 4.]
-        '''
-        # Benjamin Commeau's edit:
-        N = random.randint(0, flags.big)
-        '''
-        N = random_python('randint', 0, flags.big)
-        if N !=0: bboxes_num += 1
+        # 2022-08-17 - Switched to seeded randomizer
+        N = randomizer.randint(0, flags.big)
+        if N != 0: bboxes_num += 1
         for _ in range(N):
-            '''
-            # Benjamin Commeau's edit:
-            ratio = random.choice(ratios)
-            idx = random.randint(0, 54999)
-            '''
-            ratio = random_python('choice', ratios)
-            idx = random_python('randint', 0, 54999)
+            # 2022-08-17 - Switched to seeded randomizer
+            ratio = randomizer.choice(ratios)
+            idx = randomizer.randint(0, 54999)
             data[0] = make_image(data, image_paths[idx], ratio)
 
         if bboxes_num == 0: continue
@@ -214,6 +182,5 @@ with open(flags.labels_txt, "w") as wf:
             class_ind = str(labels[i])
             annotation += ' ' + ','.join([xmin, ymin, xmax, ymax, str(class_ind)])
         image_num += 1
-        print("=> %s" %annotation)
+        print("=> %s" % annotation)
         wf.write(annotation + "\n")
-
